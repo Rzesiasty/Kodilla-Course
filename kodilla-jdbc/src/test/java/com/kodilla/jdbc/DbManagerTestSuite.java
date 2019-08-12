ccb4c18ed2d1ef12123fa20acd28a41cfc659b1e
Rzesiasty <andrzej.splewinski@gmail.com>
2019-08-12 05:51:16 +0200

    Active users task

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.jdbc;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,5 +40,31 @@ public class DbManagerTestSuite {
             statement.close();
 
         Assert.assertEquals(5, counter);
+    }
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "select U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER\n" +
+                "from POSTS i join USERS U on i.USER_ID = U.id\n" +
+                "group by USER_ID\n" +
+                "having count(*) >= 2\n" +
+                "order by U.LASTNAME;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()){
+            System.out.println(rs.getInt("POSTS_NUMBER") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter ++;
+        }
+        rs.close();
+            statement.close();
+        Assert.assertEquals(2, counter);
     }
 }
