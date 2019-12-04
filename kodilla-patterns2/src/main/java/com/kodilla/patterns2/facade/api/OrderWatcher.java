4be3042c8b47943afc33e49db37945ee76d51fe6
Andrzej <andrzej.splewinski@gmail.com>
2019-12-04 22:37:20 +0100

    update aspect programming

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderWatcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderWatcher.java
@@ -8,14 +8,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Aspect
 @Component
 public class OrderWatcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderWatcher.class);
 
-    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processorOrder(..))" + "&& target(object)")
-    public void logEvent(Object object) {
-        LOGGER.info("Class: " + object.getClass().getName());
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processorOrder(..))" + "&& target(object) && args(order, userId)")
+    public void logEvent(Object object, OrderDto order, Long userId) {
+        LOGGER.info("Class: " + object.getClass().getName() + " items id: " + order.getItems().stream()
+                .map(n -> n.getProductId().toString())
+                .collect(Collectors.joining(" ,"))
+                + " User id " + userId.toString());
         LOGGER.info("Method processorOrder is activated");
     }
 
