08bb5c471948b2940f6e1a5d9185e578d6b0b294
Andrzej <andrzej.splewinski@gmail.com>
2020-01-11 15:54:39 +0100

    checking the operation of the component procedure UpdateBestsellers

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -29,4 +29,26 @@ public class StoredProcTestSuite {
         }
         Assert.assertEquals(0, howMany);
     }
+
+    @Test
+    public void updateBestSellers() throws SQLException{
+        //Given
+        DbManager manager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=2";
+        Statement statement = manager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=2";
+        ResultSet resultSet = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (resultSet.next()){
+            howMany = resultSet.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0, howMany);
+    }
 }
