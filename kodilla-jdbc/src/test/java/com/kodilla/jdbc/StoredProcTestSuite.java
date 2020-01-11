30d57c3c9a61e36a03469374f3fb85a52f7f84b3
Andrzej <andrzej.splewinski@gmail.com>
2020-01-11 15:41:32 +0100

    Calling a stored procedure from Java code

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevel() throws SQLException {
+        //Given
+        DbManager manager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = manager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateLevels()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet res = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (res.next()) {
+            howMany = res.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0, howMany);
+    }
+}
